a26b11a4bebae129c0bee722ff605c1a074da24e
Nicolomecca <nico97.mecca@gmail.com>
2024-11-05 12:26:10 +0100

    up

--- W6_d2/src/main/java/Nicolo_Mecca/W6_d2/excepetions/ExceptionsHandler.java
+++ W6_d2/src/main/java/Nicolo_Mecca/W6_d2/excepetions/ExceptionsHandler.java
@@ -2,6 +2,7 @@ package Nicolo_Mecca.W6_d2.excepetions;
 
 import Nicolo_Mecca.W6_d2.payloads.ErrorsResponseDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,6 +22,12 @@ public class ExceptionsHandler {
     @ResponseStatus(HttpStatus.UNAUTHORIZED) // 401
     public ErrorsResponseDTO handleUnauthorized(UnauthorizedException ex) {
         return new ErrorsResponseDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN) // 403
+    public ErrorsResponseDTO handleForbidden(AuthorizationDeniedException ex) {
+        return new ErrorsResponseDTO("Non hai i permessi per accedere", LocalDateTime.now());
     }
 
     @ExceptionHandler(NotFoundException.class)
