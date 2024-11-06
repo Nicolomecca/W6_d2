0dc55944106b1b9f17130581879010dd20a37d83
Nicolomecca <nico97.mecca@gmail.com>
2024-11-06 14:42:13 +0100

    up

--- W6_d2/src/main/java/Nicolo_Mecca/W6_d2/repositories/ViaggioRepository.java
+++ W6_d2/src/main/java/Nicolo_Mecca/W6_d2/repositories/ViaggioRepository.java
@@ -4,4 +4,6 @@ import Nicolo_Mecca.W6_d2.entities.Viaggio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ViaggioRepository extends JpaRepository<Viaggio, Long> {
+
 }
+
